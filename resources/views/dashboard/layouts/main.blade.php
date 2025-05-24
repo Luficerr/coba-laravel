0e7f907d9220b71406d221080fea668cfd61c76f
Lutfi Faiz Ziyadatullah <lutfifaizziyadatullah@gmail.com>
2025-05-24 23:09:14 +0700

    Deskripsi perubahan

--- resources/views/dashboard/layouts/main.blade.php
+++ resources/views/dashboard/layouts/main.blade.php
@@ -15,6 +15,9 @@
     <link rel="stylesheet" type="text/css" href="/css/trix.css">
     <script type="text/javascript" src="/js/trix.js"></script>
 
+    {{-- <link rel="stylesheet" type="text/css" href="https://unpkg.com/trix@2.0.8/dist/trix.css">
+    <script type="text/javascript" src="https://unpkg.com/trix@2.0.8/dist/trix.umd.min.js"></script> --}}
+
     <script>
       trix-toolbar [data-trix-button-group="file-tools"] {
         display: none;
